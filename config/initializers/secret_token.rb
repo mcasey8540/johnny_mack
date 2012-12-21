5796a342ce7c74cc3433f6c04416e8e1db4807f0
Michael Casey <mcasey8540@gmail.com>
2012-12-21 17:04:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JohnnyMack::Application.config.secret_token = '719bb9ff89be5923a3b1675963b1a14f592a77a97e39292005a8766553ba01603f74d4b4586912bc958f9c8f81a0d14182635a14ebf6dbbfe04b4dd9b816d3c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+JohnnyMack::Application.config.session_store :cookie_store, key: '_johnny_mack_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# JohnnyMack::Application.config.session_store :active_record_store
